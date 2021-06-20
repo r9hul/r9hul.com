91fa98f209423dcdebcd5f4fa0bd1c2ea3ae3db8
Daksh Miglani <hello@dak.sh>
2021-06-20 23:54:46 +0530

    feat: 🎸 changed it for r9hul.com

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '4c5d4c9403084acc8cbf07ccb67d8a49',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Rahul Agarwal',
+  domain: 'r9hul.com',
+  author: 'Rahul Agarwal',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Rahul\'s Home Page',
+  socialImageTitle: 'Rahul Agarwal',
+  socialImageSubtitle: 'Hello I\'m rahul! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'r9hul',
+  github: '',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
